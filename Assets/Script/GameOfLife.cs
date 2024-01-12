0523fa5b68f8b09ebddacfbb367006784c38e49f
Frogtreee <142871308+frogtreee@users.noreply.github.com>
2024-01-12 10:15:07 +0900

    Unity Files

---
+++ Assets/Script/GameOfLife.cs
@@ -0,0 +1,126 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class GameOfLife : MonoBehaviour
+{
+    [SerializeField] private Tilemap currentState;
+    [SerializeField] private Tilemap nextState;
+    [SerializeField] private Tile aliveTile;
+    [SerializeField] private Tile deadTile;
+    [SerializeField] private Pattern pattern;
+     [SerializeField]private float updateInterval = 0.05f;
+
+     private HashSet<Vector3Int> aliveCells; 
+     private HashSet<Vector3Int> cellsToCheck;
+
+     public int population {get; private set; }
+     public int iterations { get; private set; }
+     public float time {get; private set; }
+
+     private void Awake(){
+   
+         aliveCells = new HashSet<Vector3Int>();
+         cellsToCheck = new HashSet<Vector3Int>();
+     }
+
+     private void Start(){
+    
+        SetPattern(pattern);
+     }
+
+     private void SetPattern(Pattern pattern){
+
+        Clear();
+
+        Vector2Int center =  pattern.GetCenter();
+         for(int i =0; i < pattern.cells.Length; i++){
+            Vector3Int cell = (Vector3Int)(pattern.cells[i] - center);
+            currentState.SetTile(cell, aliveTile); 
+            aliveCells.Add(cell);
+         }
+
+         population = aliveCells.Count;
+     }
+
+     private void Clear(){
+        currentState.ClearAllTiles();
+        nextState.ClearAllTiles(); 
+        aliveCells.Clear();
+        cellsToCheck.Clear();
+        population = 0;
+        iterations = 0;
+        time = 0f;
+
+     }
+
+     private void OnEnable(){
+
+        StartCoroutine(Simulate());
+     }
+
+     private IEnumerator Simulate(){
+
+        var interval = new WaitForSeconds(updateInterval);
+        while(enabled){
+            UpdateState();
+            population = aliveCells.Count;
+            iterations++;
+            time += updateInterval;
+            yield return interval;
+        }
+     }
+
+   private void UpdateState(){
+
+      cellsToCheck.Clear();
+      foreach(Vector3Int cell in aliveCells){
+         for(int x = -1; x <= 1; x++){
+            for(int y = -1; y <= 1; y++){
+               cellsToCheck.Add(cell + new Vector3Int(x,y));
+            }
+         }
+      }
+
+      foreach(Vector3Int cell in cellsToCheck){
+         int neighbor = CountNeighbors(cell);
+         bool alive = IsAlive(cell);
+         
+         if(!alive && neighbor == 3){
+            nextState.SetTile(cell, aliveTile);
+            aliveCells.Add(cell);
+         }else if(alive && (neighbor < 2 || neighbor > 3)){
+            nextState.SetTile(cell, deadTile);
+            aliveCells.Remove(cell);
+         }else{
+            nextState.SetTile(cell, currentState.GetTile(cell));
+         }
+      }
+
+      Tilemap temp = currentState;
+      currentState = nextState;
+      nextState = temp;
+      nextState.ClearAllTiles();
+     }
+   private int CountNeighbors(Vector3Int cell){
+
+      int count = 0;
+
+      for(int x = -1; x <= 1; x++){
+         for(int y = -1; y <= 1; y++){
+            Vector3Int neighbor = cell + new Vector3Int(x,y,0);
+            if(x == 0 && y == 0){
+               continue;
+            }else if(IsAlive(neighbor)){
+               count++;
+            }
+         }
+      }
+      return count;
+   }
+   private bool IsAlive(Vector3Int cell){
+      return currentState.GetTile(cell) == aliveTile;
+   }
+}
